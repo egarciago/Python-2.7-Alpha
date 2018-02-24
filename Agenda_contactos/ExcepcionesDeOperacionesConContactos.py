08cb1e93bbc83b2bd15546a4338cc65761dc54cc
egarciago <eliezer.ggz@gmail.com>
2018-02-23 23:02:21 -0400

    Changes in Exception class

--- Agenda_contactos/ExcepcionesDeOperacionesConContactos.py
+++ Agenda_contactos/ExcepcionesDeOperacionesConContactos.py
@@ -2,10 +2,3 @@ class AdministradorDeExcepciones(Exception):
 	def __init__(self, *args, **kwargs):
 		Exception.__init__(self,*args, **kwargs)
 
-	def controlarErroresAlListar(self):
-
-	def controlarErroresAlInsertar(self):
-
-	def controlarErroresAlEditar(self):
-
-	def controlarErroresAlEliminar(self):
