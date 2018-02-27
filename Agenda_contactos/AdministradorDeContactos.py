2c5dc62041e5bfd11fdceed930cff831fea4ca95
egarciago <eliezer.ggz@gmail.com>
2018-02-26 23:56:59 -0400

    Change in classes

--- Agenda_contactos/AdministradorDeContactos.py
+++ Agenda_contactos/AdministradorDeContactos.py
@@ -8,20 +8,22 @@ class AdministradorDeContactos:
 	def listar(self):
 		return self.contactos	
 
-	def agregar(self, nombre, telefono):
-		contacto = {
-			'Nombre': nombre, 
-			'Telefono': telefono
+	def agregar(self, contacto):
+		contactoDic = {
+			'Id': contacto.getId
+			'Nombre': contacto.getNombre, 
+			'Telefono': contacto.getTelefono
 		}
-		self.contactos.append(contacto)
+		self.contactos.append(contactoDic)
 	
-	def editar(self, posicionContacto, nombre, telefono):
-		contacto = {
-			'Nombre': nombre, 
-			'Telefono': telefono
+	def editar(self, posicionContacto, contacto):
+		contactoDic = {
+			'Nombre': contacto.getNombre, 
+			'Telefono': contactogetTelefono
 		}
 		self.contactos.pop(posicionContacto)
-		self.contactos.insert(id, contacto)
+		self.contactos.insert(id, contactoDic)
 		
 	def eliminar(self, posicionContacto):
 		self.contactos.pop(posicionContacto)
+
--- Agenda_contactos/Contacto.py
+++ Agenda_contactos/Contacto.py
@@ -1,9 +1,16 @@
 class Contacto:
 
-	def __init__(self, nombre, telefono):
+	def __init__(self, id, nombre, telefono):
+		self.id = id
 		self.nombre = nombre
 		self.telefono = telefono
 	
+	def getId(self):
+		return self.id
+
+	def setId(self, id):
+		self.id = id
+		
 	def getNombre(self):
 		return self.nombre
 	
