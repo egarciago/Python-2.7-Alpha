484297cd0f919a69eb4cdb2be1efb51705ccbe3e
egarciago <eliezer.ggz@gmail.com>
2018-03-15 22:31:57 -0400

    update classes

--- Agenda_contactos/Datos/Entidades/Contacto.py
+++ Agenda_contactos/Datos/Entidades/Contacto.py
@@ -1,8 +1,9 @@
 class Contacto:
 
-	def __init__(self, nombre, telefono):
+	def __init__(self, nombre, telefono, nota):
 		self.__nombre = nombre
 		self.__telefono = telefono
+		self.__nota  nota
 
 	def getNombre(self):
 		return self.__nombre
@@ -16,4 +17,9 @@ class Contacto:
 	def setTelefono(self, telefono):
 		self.__telefono = telefono
 
+	def getNota(self):
+		return self.__nota
+		
+	def setNota(self, nota):
+		self.__nota = nota
 	
